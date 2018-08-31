5266194a34a7fbfdf230b058b4575b8fb0eadb7c
Christopher Tran <christopher.long.tran@gmail.com>
2018-08-31 13:29:25 -0400

    created stump

---
+++ tracker/src/Pages/Stump.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Stump extends Component {
+  render () {
+    return (
+      <div className='Stump'>
+        <h1>This is the stump of the page</h1>
+      </div>
+    )
+  }
+}
+
+export default Stump
